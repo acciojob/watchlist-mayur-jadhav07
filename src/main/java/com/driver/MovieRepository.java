2ba896f1f91893584ea83fc6e18e58e45dfea187
mayur-jadhav07 <jmayur309@gmail.com>
2024-03-02 12:56:41 +0530

    WatchList assignment completed

--- src/main/java/com/driver/MovieRepository.java
+++ src/main/java/com/driver/MovieRepository.java
@@ -19,28 +19,43 @@ public class MovieRepository {
 
     public void saveMovie(Movie movie){
         // your code here
+        movieMap.put(movie.getName(), movie);
     }
 
     public void saveDirector(Director director){
         // your code here
+        directorMap.put(director.getName(), director);
     }
 
     public void saveMovieDirectorPair(String movie, String director){
         if(movieMap.containsKey(movie) && directorMap.containsKey(director)){
             // your code here
+            // Update the mapping between director and movies
+            List<String> moviesByDirector = directorMovieMapping.getOrDefault(director, new ArrayList<>());
+            moviesByDirector.add(movie);
+            directorMovieMapping.put(director, moviesByDirector);
         }
     }
 
     public Movie findMovie(String movie){
         // your code here
+        if(movieMap.containsKey(movie)){
+            return movieMap.get(movie);
+        }
+        return null;
     }
 
     public Director findDirector(String director){
         // your code here
+        if(directorMap.containsKey(director)){
+            return directorMap.get(director);
+        }
+        return null;
     }
 
     public List<String> findMoviesFromDirector(String director){
         // your code here
+        return directorMovieMapping.getOrDefault(director, new ArrayList<>());
     }
 
     public List<String> findAllMovies(){
@@ -49,9 +64,14 @@ public class MovieRepository {
 
     public void deleteDirector(String director){
         // your code here
+        directorMap.remove(director);
+        // Remove the director from the movie-director mapping
+        directorMovieMapping.remove(director);
     }
 
     public void deleteAllDirector(){
         // your code here
+        directorMap.clear();
+        directorMovieMapping.clear();
     }
 }
